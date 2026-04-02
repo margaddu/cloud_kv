65e030c5d1f610469874b0f06401530816b0576e
Margad-Erdene Tegshbayar <margaderdene1402@gmail.com>
2026-04-02 14:03:03 -0500

    Persistence: None, Logging: log_op with log_mutex on every request

--- src/main.c
+++ src/main.c
@@ -23,35 +23,35 @@ int keep_going = 1;
 
 pthread_rwlock_t rwlock;
 // int writers_waiting = 0; // Don't get reader locks if writers are waiting?
-// static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
-// static const char *LOG_FILE = "server.log";
+static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+static const char *LOG_FILE = "server.log";
 
 void handle_sigint(int signo) {
     keep_going = 0;
 }
 //Timestamp/Logging function
-// static void log_op(const char *op, const char *key, const char *status, const char *value_opt) {
-//     time_t now = time(NULL);
-//     struct tm tm_now;
-//     localtime_r(&now, &tm_now);
+static void log_op(const char *op, const char *key, const char *status, const char *value_opt) {
+    time_t now = time(NULL);
+    struct tm tm_now;
+    localtime_r(&now, &tm_now);
 
-//     char ts[64];
-//     strftime(ts, sizeof(ts), "%Y-%m-%d %H:%M:%S", &tm_now);
+    char ts[64];
+    strftime(ts, sizeof(ts), "%Y-%m-%d %H:%M:%S", &tm_now);
 
-//     pthread_mutex_lock(&log_mutex);
+    pthread_mutex_lock(&log_mutex);
 
-//     FILE *f = fopen(LOG_FILE, "a");
-//     if (f) {
-//         if (value_opt) {
-//             fprintf(f, "[%s] %s key=%s value=%s status=%s\n", ts, op, key, value_opt, status);
-//         } else {
-//             fprintf(f, "[%s] %s key=%s status=%s\n", ts, op, key, status);
-//         }
-//         fclose(f);
-//     }
+    FILE *f = fopen(LOG_FILE, "a");
+    if (f) {
+        if (value_opt) {
+            fprintf(f, "[%s] %s key=%s value=%s status=%s\n", ts, op, key, value_opt, status);
+        } else {
+            fprintf(f, "[%s] %s key=%s status=%s\n", ts, op, key, status);
+        }
+        fclose(f);
+    }
 
-//     pthread_mutex_unlock(&log_mutex);
-// }
+    pthread_mutex_unlock(&log_mutex);
+}
 
 typedef struct {
     connection_queue_t* queue;
@@ -83,8 +83,8 @@ void *thread_task(void *args) {
             kv_store_put(kv_store, req.key, req.val);
 
             pthread_rwlock_unlock(&rwlock);
-            kv_store_save_to_disk(kv_store); // Saves to disk for persistence
-            // log_op("PUT", req.key, "OK", req.val); //Display timestamp for PUT
+            // kv_store_save_to_disk(kv_store); // Saves to disk for persistence
+            log_op("PUT", req.key, "OK", req.val); //Display timestamp for PUT
             // printf("User PUT: Key=%s, Val=%s\n", req.key, req.val);
 
             respond_success(client_fd);
@@ -97,10 +97,10 @@ void *thread_task(void *args) {
             char* data = kv_store_get(kv_store, req.key);
 
             if (data == NULL) {
-                // log_op("GET", req.key, "NOT_FOUND", NULL); //Display timestamp for GET
+                log_op("GET", req.key, "NOT_FOUND", NULL); //Display timestamp for GET
                 respond_not_found(client_fd);
             } else {
-                // log_op("GET", req.key, "OK", data); //Display timestamp for GET
+                log_op("GET", req.key, "OK", data); //Display timestamp for GET
                 respond_with_payload(client_fd, (uint8_t*)data, strlen(data));
             }
 
@@ -114,12 +114,12 @@ void *thread_task(void *args) {
             pthread_rwlock_unlock(&rwlock);
 
             if(deleted==1){
-                kv_store_save_to_disk(kv_store); // Saves to disk for persistence
-                // log_op("DEL", req.key, "OK", NULL); //Display timestamp for DEL
+                // kv_store_save_to_disk(kv_store); // Saves to disk for persistence
+                log_op("DEL", req.key, "OK", NULL); //Display timestamp for DEL
                 respond_success(client_fd);
             }
             else{
-                // log_op("DEL", req.key, "NOT_FOUND", NULL); //Display timestamp for DEL
+                log_op("DEL", req.key, "NOT_FOUND", NULL); //Display timestamp for DEL
                 respond_not_found(client_fd);
             }
         }
